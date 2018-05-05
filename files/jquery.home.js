de1e5fe7af30c8a3cbb63daffa2099f448fd1346
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-05-05 20:53:03 +0900

    mobile profile

---
+++ files/jquery.home.js
@@ -0,0 +1,9 @@
+/* LOGOBOX HEIGHT */
+$(document).ready(function () {
+	hsize = $(window).height();
+	$("#logobox").css("height", hsize + "px");
+});
+$(window).resize(function () {
+	hsize = $(window).height();
+	$("#logobox").css("height", hsize + "px");
+});
