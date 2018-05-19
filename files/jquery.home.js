68139ab1697b0aa9345f8016ed17a22aaa977f68
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-05-19 14:34:20 +0900

    Add

--- files/jquery.home.js
+++ files/jquery.home.js
@@ -7,3 +7,11 @@ $(window).resize(function () {
     hsize = $(window).height();
     $("#logobox").css("height", hsize + "px");
 });
+$(document).ready(function () {
+    var scrollOff = function (e) {
+        e.preventDefault();
+    }
+    document.addEventListener('touchmove', scrollOff, {
+        passive: false
+    });
+});
